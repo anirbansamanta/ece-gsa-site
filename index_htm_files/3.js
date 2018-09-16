ea6b622c52d7778b88994dd39bc28ce33e743141
Anirban Samanta <43252387+anirbansamanta@users.noreply.github.com>
2018-09-16 13:42:53 -0700

    initial file upload

---
+++ index_htm_files/3.js
@@ -0,0 +1,42 @@
+window.local_imageGallerySimple_v11_6_data = [
+{
+ border: "4",
+ cols: "3",
+ effect: "1",
+ popupsize: "750",
+ rotation: "-360",
+ shadowblur: "4",
+ shadowdim: "4",
+ trans: "0.3"
+},
+{
+ image: "index_htm_files/192.jpg"
+},
+{
+ image: "index_htm_files/193.jpg"
+},
+{
+ image: "index_htm_files/194.jpg"
+},
+{
+ image: "index_htm_files/195.jpg"
+},
+{
+ image: "index_htm_files/196.jpg"
+},
+{
+ image: "index_htm_files/197.jpg"
+},
+{
+ image: "index_htm_files/198.jpg"
+},
+{
+ image: "index_htm_files/199.jpg"
+},
+{
+ image: "index_htm_files/200.jpg"
+},
+{
+ image: "index_htm_files/201.jpg"
+}
+];
