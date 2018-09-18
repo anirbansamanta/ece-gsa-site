d3428750ef9718710a340b3f66e579a31dd9032e
Anirban Samanta <43252387+anirbansamanta@users.noreply.github.com>
2018-09-18 16:47:10 -0700

    updated info, images

---
+++ home_htm_files/2.js
@@ -0,0 +1,42 @@
+window.local_imageGallerySimple_v11_9_data = [
+{
+ border: "4",
+ cols: "4",
+ effect: "1",
+ popupsize: "750",
+ rotation: "-360",
+ shadowblur: "4",
+ shadowdim: "4",
+ trans: "0.3"
+},
+{
+ image: "home_htm_files/192.jpg"
+},
+{
+ image: "home_htm_files/193.jpg"
+},
+{
+ image: "home_htm_files/194.jpg"
+},
+{
+ image: "home_htm_files/195.jpg"
+},
+{
+ image: "home_htm_files/196.jpg"
+},
+{
+ image: "home_htm_files/197.jpg"
+},
+{
+ image: "home_htm_files/198.jpg"
+},
+{
+ image: "home_htm_files/199.jpg"
+},
+{
+ image: "home_htm_files/200.jpg"
+},
+{
+ image: "home_htm_files/201.jpg"
+}
+];
