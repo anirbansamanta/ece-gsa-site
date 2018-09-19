bbf8c1e024e68c1d07402a1af9eee7acdd56b098
Anirban Samanta <43252387+anirbansamanta@users.noreply.github.com>
2018-09-18 17:24:52 -0700

    Site_v2

---
+++ home_htm_files/1.js
@@ -0,0 +1,42 @@
+window.local_imageGallerySimple_v11_1_data = [
+{
+ border: "4",
+ cols: "4",
+ effect: "1",
+ popupsize: "750",
+ rotation: "-360",
+ shadowblur: "4",
+ shadowdim: "4",
+ trans: "0.3"
+},
+{
+ image: "home_htm_files/799.jpg"
+},
+{
+ image: "home_htm_files/800.jpg"
+},
+{
+ image: "home_htm_files/801.jpg"
+},
+{
+ image: "home_htm_files/802.jpg"
+},
+{
+ image: "home_htm_files/803.jpg"
+},
+{
+ image: "home_htm_files/804.jpg"
+},
+{
+ image: "home_htm_files/805.jpg"
+},
+{
+ image: "home_htm_files/806.jpg"
+},
+{
+ image: "home_htm_files/807.jpg"
+},
+{
+ image: "home_htm_files/808.jpg"
+}
+];
