163df4dd267408b3e8b5ade5eb4529fc3412a95e
Anirban Samanta <43252387+anirbansamanta@users.noreply.github.com>
2018-09-18 20:05:17 -0700

    ecegsa_site_v0.6

--- home_htm_files/3.js
+++ home_htm_files/3.js
@@ -10,33 +10,33 @@ window.local_imageGallerySimple_v11_3_data = [
  trans: "0.3"
 },
 {
- image: "home_htm_files/799.jpg"
+ image: "home_htm_files/1322.jpg"
 },
 {
- image: "home_htm_files/800.jpg"
+ image: "home_htm_files/1323.jpg"
 },
 {
- image: "home_htm_files/801.jpg"
+ image: "home_htm_files/1324.jpg"
 },
 {
- image: "home_htm_files/802.jpg"
+ image: "home_htm_files/1325.jpg"
 },
 {
- image: "home_htm_files/803.jpg"
+ image: "home_htm_files/1326.jpg"
 },
 {
- image: "home_htm_files/804.jpg"
+ image: "home_htm_files/1327.jpg"
 },
 {
- image: "home_htm_files/805.jpg"
+ image: "home_htm_files/1328.jpg"
 },
 {
- image: "home_htm_files/806.jpg"
+ image: "home_htm_files/1329.jpg"
 },
 {
- image: "home_htm_files/807.jpg"
+ image: "home_htm_files/1330.jpg"
 },
 {
- image: "home_htm_files/808.jpg"
+ image: "home_htm_files/1331.jpg"
 }
 ];
