af87055823be1a93c26d2b0e7a000113938662f7
Anirban Samanta <43252387+anirbansamanta@users.noreply.github.com>
2019-09-21 13:32:51 -0700

    GSA_2019
    GSA_2018 removed
    GSA_2019 added

---
+++ ECE-GSA_Site_2019_htm_files/1.js
@@ -0,0 +1,48 @@
+window.local_imageGallerySimple_v11_8_data = [
+{
+ border: "4",
+ cols: "4",
+ effect: "0",
+ popupsize: "750",
+ rotation: "-360",
+ shadowblur: "4",
+ shadowdim: "4",
+ trans: "0.3"
+},
+{
+ image: "ECE-GSA_Site_2019_htm_files/1980.jpg"
+},
+{
+ image: "ECE-GSA_Site_2019_htm_files/1981.jpg"
+},
+{
+ image: "ECE-GSA_Site_2019_htm_files/1982.jpg"
+},
+{
+ image: "ECE-GSA_Site_2019_htm_files/1983.jpg"
+},
+{
+ image: "ECE-GSA_Site_2019_htm_files/1984.jpg"
+},
+{
+ image: "ECE-GSA_Site_2019_htm_files/1985.jpg"
+},
+{
+ image: "ECE-GSA_Site_2019_htm_files/1986.jpg"
+},
+{
+ image: "ECE-GSA_Site_2019_htm_files/1987.jpg"
+},
+{
+ image: "ECE-GSA_Site_2019_htm_files/1988.jpg"
+},
+{
+ image: "ECE-GSA_Site_2019_htm_files/1989.jpg"
+},
+{
+ image: "ECE-GSA_Site_2019_htm_files/1990.jpg"
+},
+{
+ image: "ECE-GSA_Site_2019_htm_files/1991.jpg"
+}
+];
---
+++ ECE-GSA_Site_2019_htm_files/8.js
@@ -0,0 +1,24 @@
+window.local_imageGallerySimple_v11_17_data = [
+{
+ border: "4",
+ cols: "4",
+ effect: "0",
+ popupsize: "750",
+ rotation: "-360",
+ shadowblur: "4",
+ shadowdim: "4",
+ trans: "0.3"
+},
+{
+ image: "ECE-GSA_Site_2019_htm_files/1327.jpg"
+},
+{
+ image: "ECE-GSA_Site_2019_htm_files/1329.jpg"
+},
+{
+ image: "ECE-GSA_Site_2019_htm_files/1330.jpg"
+},
+{
+ image: "ECE-GSA_Site_2019_htm_files/1331.jpg"
+}
+];
