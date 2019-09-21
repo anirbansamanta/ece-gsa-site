bc056f88e003555c0d7ca9a56421dbf089b1ff16
Anirban Samanta <43252387+anirbansamanta@users.noreply.github.com>
2019-09-21 15:11:47 -0700

    Test_Back_7

---
+++ ece_gsa_home_2019_htm_files/3.js
@@ -0,0 +1,36 @@
+window.local_imageGallerySimple_v11_32_data = [
+{
+ border: "4",
+ cols: "4",
+ effect: "0",
+ popupsize: "750",
+ rotation: "-360",
+ shadowblur: "4",
+ shadowdim: "4",
+ trans: "0.3"
+},
+{
+ image: "ece_gsa_home_2019_htm_files/2280.jpg"
+},
+{
+ image: "ece_gsa_home_2019_htm_files/2281.jpg"
+},
+{
+ image: "ece_gsa_home_2019_htm_files/2282.jpg"
+},
+{
+ image: "ece_gsa_home_2019_htm_files/2283.jpg"
+},
+{
+ image: "ece_gsa_home_2019_htm_files/2284.jpg"
+},
+{
+ image: "ece_gsa_home_2019_htm_files/2285.jpg"
+},
+{
+ image: "ece_gsa_home_2019_htm_files/2286.jpg"
+},
+{
+ image: "ece_gsa_home_2019_htm_files/2287.jpg"
+}
+];
